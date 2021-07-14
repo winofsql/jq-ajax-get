b615c22219a4248bb28ac0448eea16c1a5c5a053
sworc <lightbox@nifty.com>
2021-07-14 21:02:17 +0900

    Add files via upload

---
+++ pg-06-mysql-list-del/form-action-json-mysql-del.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting( E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED );
+session_cache_limiter('nocache');
+session_start();
+header( "Access-Control-Allow-Origin: *" );
+header( "Content-Type: application/json; charset=utf-8" );
+
+// ***********************************
+// データーベース接続情報
+// ***********************************
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "lightbox";
+$connect_string = "mysql:host={$host};dbname={$dbname}";
+
+// ***********************************
+// 接続
+// ***********************************
+$pdo = new PDO( $connect_string, $user, $password );
+
+// ***********************************
+// SQL 実行
+// ***********************************
+$sql = <<<QUERY
+delete 
+    from 社員マスタ
+    where 社員コード = :code
+QUERY;
+
+$statement = $pdo->prepare($sql);
+
+foreach( $_GET["code"] as $value ) {
+
+    $statement->bindValue(":code", $value, PDO::PARAM_STR );
+    $statement->execute();
+    
+}
+
+
+// ***********************************
+// JSON データを作成
+// ***********************************
+$json = new stdClass;
+$json->get = $_GET;
+
+// ***********************************
+// ブラウザへ返す
+// JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT はデバッグ用
+// ***********************************
+//print json_encode( $json );
+print json_encode( $json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT );
+
+?>
