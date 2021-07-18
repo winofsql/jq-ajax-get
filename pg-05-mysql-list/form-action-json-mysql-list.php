3882119511402ae2f7d63ad86e43e564263b87a8
sworc <lightbox@nifty.com>
2021-07-18 14:36:27 +0900

    Update form-action-json-mysql-list.php

--- pg-05-mysql-list/form-action-json-mysql-list.php
+++ pg-05-mysql-list/form-action-json-mysql-list.php
@@ -13,6 +13,9 @@ $user = "root";
 $password = "";
 $dbname = "lightbox";
 $connect_string = "mysql:host={$host};dbname={$dbname}";
+if ( file_exists( $_SERVER['DOCUMENT_ROOT'] . "/app/connect.php" ) ){
+	require_once( $_SERVER['DOCUMENT_ROOT'] . "/app/connect.php" );
+}
 
 // ***********************************
 // 接続
